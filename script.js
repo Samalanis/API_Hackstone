0cbfb78bd2f8bb54eb2be13b21764974c87afecf
Samalanis <Samalanis23@outlook.com>
2019-12-03 01:07:12 +0000

    Added photos for the list - working on next-button problem with incorrectly displaying text

--- script.js
+++ script.js
@@ -1,30 +1,34 @@
 // api key and base url
-const baseUrl = "https://api.themoviedb.org/3/discover/movie?api_key";
-const apiKey = "b51c1e917ec47e74e150682c58822b2b";
-
+//const baseUrl = "https://api.themoviedb.org/3/discover/movie?api_key";
+//const apiKey = "b51c1e917ec47e74e150682c58822b2b";
+//https://image.tmdb.org/t/p/w500/r5WnfZPYAVhBA9FuZGn6THWaGHD.jpg
 //https://api.themoviedb.org/3/discover/movie?api_key=b51c1e917ec47e74e150682c58822b2b&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=35
 
 // a function to get the value from when the user submits
-function pullValue(){
+let pageCount = 1;
+
+function pullValue(count){
 $('#submitbox').on('click', event => {
   event.preventDefault();
 let genValue = $('#genreList').val();
 console.log(` id: ${genValue} `);
-$(queryPull(genValue));
+queryPull(genValue, pageCount);
  })
 } 
 
 // a function to que the string to call 
-function queryPull(genValue) {
-  let urlGenre = `https://api.themoviedb.org/3/discover/movie?api_key=b51c1e917ec47e74e150682c58822b2b&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=${genValue}`;
+function queryPull(a, b) {
+  console.log(b);
+  console.log('queryPull is running');
+    let urlGenre = `https://api.themoviedb.org/3/discover/movie?api_key=b51c1e917ec47e74e150682c58822b2b&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${b}&with_genres=${a}`;
   //console.log(urlGenre);
-  $(loadJson(urlGenre));
+  loadJson(urlGenre);
 }
 
 // a function to make the call using fetch and submitting the key
-function loadJson(urlGenre){
-  console.log('loadJson running');
-  fetch(urlGenre)
+function loadJson(url){
+  console.log('loadJson is running');
+  fetch(url)
   .then(response => {
     if(response.ok) {
       return response.json();
@@ -39,18 +43,32 @@ function loadJson(urlGenre){
 
 // a function to display the results 
 function resultsJson(responseJson) {
+  console.log(responseJson);
+  $('#displayResults').removeClass('hidden').empty()
+  $('#displayResults').append('<button type="button" value="next-page" id="next-button">next page</button>')
+  nextPagePull();
   for (let i = 0; i < responseJson.results.length; ++i) {
-
-  $('#displayResults').removeClass('hidden').empty().append('<button type="button" value="next-page" id="next-button">next page</button>')
-  $('#displayResults').prepend(`<h3>${responseJson.results[i].title}</h3></br><p>${responseJson.results[i].overview}`);
+  $('#displayResults').prepend(`<h3>${responseJson.results[i].title}</h3></br>
+  <img src="https://image.tmdb.org/t/p/w500/${responseJson.results[i].poster_path}"><p>${responseJson.results[i].overview}`);
   }
 }
 
+
+// function for handling the next page pulls 
+function nextPagePull() {
+  $('#next-button').on('click', event => {
+    pageCount ++;
+    console.log(pageCount);
+    console.log('nextPagePull runs');
+    queryPull(pageCount);
+  });
+}
 
 
 
 // a function to render functions 
 function renderFunctions() {
-$(pullValue)
+pullValue();
+nextPagePull();
 }
-$(renderFunctions);
+renderFunctions();
