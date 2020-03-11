5cafe8b0ffae239f8345aac4ef56dc0422ef2931
Samalanis <51542491+Samalanis@users.noreply.github.com>
2020-03-10 17:49:17 -0700

    Final Design
    Made edits to some of the j-query

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// API KEY
+const apikey = 'b51c1e917ec47e74e150682c58822b2b';
 
 // a function to get the value from when the user submits
 function pullValue(){
@@ -5,6 +7,8 @@ $('#submitbox').on('click', event => {
   event.preventDefault();
 let genValue = $('#selectList').val();
 console.log(` id: ${genValue} `);
+
+// possibly a page incremenet for more results. 
 queryPull(genValue);
  })
 } 
@@ -13,7 +17,7 @@ queryPull(genValue);
 function queryPull(a) {
   console.log(a);
   console.log('queryPull is running');
-    let urlGenre = `https://api.themoviedb.org/3/discover/movie?api_key=b51c1e917ec47e74e150682c58822b2b&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=${a}`;
+    let urlGenre = `https://api.themoviedb.org/3/discover/movie?api_key=${apikey}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=${a}`;
   //console.log(urlGenre);
   loadJson(urlGenre);
 }
@@ -22,11 +26,16 @@ function queryPull(a) {
 // consolidated function to perform random method to pull a random movie
 function loadJson(url){
   console.log('loadJson is running');
-
+  $('#map').toggle();
+ 
+$('#displayResults').removeClass('resultsHide');
 let arr = [];
 
+
+
+
 while(arr.length < 3){
-    var r = Math.floor(Math.random() * 20);
+    let r = Math.floor(Math.random() * 20);
     if(arr.indexOf(r) === -1) arr.push(r);
 }
  console.log(arr);
@@ -48,45 +57,117 @@ while(arr.length < 3){
 // a function to display the results 
 function resultsJson(responseJson, randomArr) {
   console.log(responseJson.results.length);
-  $('#displayResults').removeClass('hidden').empty().css({"margin-top": "200px"});
+$('#map').empty();
+$('#displayResults').empty();
+$('#titleResults').empty();
+  $('#displayResults').removeClass('hidden').empty().css({"margin-top": "500px"});
 
   $('#displayResults').prepend(`
+
   <ul id="listUL">
-  <li><h3>${responseJson.results[randomArr[0]].title}</h3></br>
-  <div class="image-results"><img src="https://image.tmdb.org/t/p/w500/${responseJson.results[randomArr[0]].poster_path}"></div><p>${responseJson.results[randomArr[0]].overview}</li>
 
-  <li><h3>${responseJson.results[randomArr[1]].title}</h3></br>
-  <div class="image-results"><img src="https://image.tmdb.org/t/p/w500/${responseJson.results[randomArr[1]].poster_path}"></div><p>${responseJson.results[randomArr[1]].overview}</li>
+  <div class="container 1">
+  <li>
+  <div class="image-results"><img src="https://image.tmdb.org/t/p/w500/${responseJson.results[randomArr[0]].poster_path}"></div><h3 id="h3One">${responseJson.results[randomArr[0]].title}</h3></br><p>${responseJson.results[randomArr[0]].overview}</li></div>
 
-  <li><h3>${responseJson.results[randomArr[2]].title}</h3></br>
-  <div class="image-results"><img src="https://image.tmdb.org/t/p/w500/${responseJson.results[randomArr[2]].poster_path}"></div><p>${responseJson.results[randomArr[2]].overview}</li>`);
+  <div class="container 2">
+  <li>
+  <div class="image-results"><img src="https://image.tmdb.org/t/p/w500/${responseJson.results[randomArr[1]].poster_path}"></div>
+  <h3 id="h3Two">${responseJson.results[randomArr[1]].title}</h3></br><p>${responseJson.results[randomArr[1]].overview}</li></div>
 
-  $('#heading-1').append(`<i class="fa fa-arrow-circle-down" id="arrow" style="font-size:55px;color:pink"></i>>`);
 
-  $('#heading-1').append(`<section id="heading-2"><h2>It's that easy...</h2></section>`);
+  <div class="container 3">
+  <li>
+  <div class="image-results"><img src="https://image.tmdb.org/t/p/w500/${responseJson.results[randomArr[2]].poster_path}"></div><h3 id="h3Three">${responseJson.results[randomArr[2]].title}</h3></br><p>${responseJson.results[randomArr[2]].overview}</li></div></ul>`);
 
-// testing();
+  //$('#heading-1').append(`<a href="#listUL"> <i class="fa fa-arrow-circle-down" id="arrow" style="font-size:55px;color:pink"></i></a>`);
+
+  //arrowDown();
+
 }
 
 
-/*function testing() {
-  $('#submitbox').on('click', event => {
+// function for making the arrow scroll down
+function arrowDown() {
+  $('#arrow').on('click', event => {
     event.preventDefault();
     $('html, body').animate({
         scrollTop: $("#listUL").offset().top
     }, 2000);
   })
   console.log('test is running');
-}*/
-/* HTML <input type="submit" value="test" id="test">*/
-
-
-// a function to render functions 
-function renderFunctions() {
-pullValue();
 }
-renderFunctions();
 
 
+
+// function for handling the value input - movie search
+function searchMovieVal(){
+  $('#searchMovieButton').on('click', event => {
+    event.preventDefault();
+    let searchQuery = $('#searchInput').val();
+    console.log(searchQuery);
+    movieJsonSearch(searchQuery);
+  })
+}
+
+$(searchMovieVal);
+
+// function to handle the call to the API for the movie search
+function movieJsonSearch(query){
+  let queryUrl = `https://api.themoviedb.org/3/search/movie?api_key=${apikey}&language=en-US&query=${query}&page=1&include_adult=false`; 
+  console.log(queryUrl);
+
+  fetch(queryUrl)
+  .then(response => {
+    if(response.ok) {
+      return response.json();
+    }
+    throw new Error(response.statusText);
+  })
+  .then(responseJson => searchResults(responseJson))
+  .catch(err => {
+    $('#errorHandle').text(`Something went wrong: ${err.message}`);
+  });
+}
+
+// need help with this one
+// function for handling the json from the search 
+function searchResults(jsonResults) {
+console.log(jsonResults);
+$('#map').empty();
+$('#displayResults').empty();
+$('#titleResults').empty();
+$('#titleResults').css({"margin-top": "200px"});
+
+  $('#titleResults').prepend(`
+
+  <div class="container 1">
+  <div class="image-results"><img src="https://image.tmdb.org/t/p/w500/${jsonResults.results[0].backdrop_path}"></div><h3>${jsonResults.results[0].title}</h3></br><p>${jsonResults.results[0].overview}</div>
+  
+  <div class="container 2">
+    <div class="image-results"><img src="https://image.tmdb.org/t/p/w500/${jsonResults.results[1].backdrop_path}"></div><h3>${jsonResults.results[1].title}</h3></br><p>${jsonResults.results[1].overview}</div>
+
+
+  <div class="container 3">
+  <div class="image-results"><img src="https://image.tmdb.org/t/p/w500/${jsonResults.results[2].backdrop_path}"></div><h3>${jsonResults.results[2].title}</h3></br><p>${jsonResults.results[2].overview}</div>`);
+  
+
+};
+
+// function for burger menu showing and hiding
+function burgerMenu() {
+ $('#hamburgerIcon').on('hover', event => {
+   $('#hot-links').toggle();
+ })
+  
+}
+
+
+
+function renderFunc() {
+  pullValue();
+  burgerMenu();
+}
+$(renderFunc);
 
 
